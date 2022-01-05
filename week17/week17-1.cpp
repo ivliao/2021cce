66fee168a4618270dcc48d24cc2d0761c5f41c67
廖云瑄 <91170820+ivliao@users.noreply.github.com>
2022-01-05 11:21:14 +0800

    Add files via upload

---
+++ week17/week17-1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int N=20030101;
+    N=N/10;
+    printf("%d =>%d %d\n",N,N/10,N%10);
+    N=N/10;
+    printf("%d =>%d %d\n",N,N/10,N%10);
+    N=N/10;
+    printf("%d =>%d %d\n",N,N/10,N%10);
+    N=N/10;
+    printf("%d =>%d %d\n",N,N/10,N%10);
+}
